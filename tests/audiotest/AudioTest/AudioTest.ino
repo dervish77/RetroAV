bed6a81191b87e5a5618c47ca78ce239f5ca72c4
dervish77 <bslingard@yahoo.com>
2025-09-13 18:16:41 -0400

    added templates for audio test and serial test ; added 'make ide' targets

---
+++ tests/audiotest/AudioTest/AudioTest.ino
@@ -0,0 +1,34 @@
+// AudioTest - test of RetroAV audio interface
+//
+// Target: Arduino UNO
+//
+
+#include <Arduino.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+//#include <PS2KeyAdvanced.h>
+
+#include "Shield.h"
+
+
+// define global variables
+//PS2KeyAdvanced keyboard;
+
+
+// define static variables
+
+
+// Init the audio interface
+void setup() {
+
+}
+
+// Run the audio interface tests
+void loop() {
+
+
+}
+
+
+
---
+++ tests/sertest/SerTest/SerTest.ino
@@ -0,0 +1,34 @@
+// SerTest - test of RetroAV serial interface
+//
+// Target: Arduino UNO
+//
+
+#include <Arduino.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+//#include <PS2KeyAdvanced.h>
+
+#include "Shield.h"
+
+
+// define global variables
+//PS2KeyAdvanced keyboard;
+
+
+// define static variables
+
+
+// Init the serial interface
+void setup() {
+
+}
+
+// Run the serial interface tests
+void loop() {
+
+
+}
+
+
+
